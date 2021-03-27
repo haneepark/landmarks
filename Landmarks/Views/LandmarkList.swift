f8fb9aa853c5e77bdc418f8b0bb07561ecad8708
Hanee Park <hanea8199@gmail.com>
2021-03-27 16:10:27 +0900

    랜드마크 리스트 그리기

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Park Hanee on 2021/03/27.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
